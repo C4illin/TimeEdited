9146935599f7864f25a94e3d428b4f444f578111
C4illin <emrik.ostling@gmail.com>
2022-11-04 20:31:09 +0100

    initial start

--- index.js
+++ index.js
@@ -1,23 +1,52 @@
 import ical2json from 'ical2json';
-// import fs from 'fs';
+import fs from 'fs';
 import got from 'got';
 import express from 'express';
+import compression from "compression"
+import helmet from "helmet"
+import ShortUniqueId from 'short-unique-id';
+import config from './config.json' assert { type: "json" };
 const app = express()
 const port = 3000
+const uid = new ShortUniqueId({ length: 8 });
 
-const url = 'https://cloud.timeedit.net/chalmers/web/public/ri6Y036mZ55Z6hQ1W55865615Q40y4Q6Zt680ZZZX46Q627695y0nZ65QZA1D6C7tZCD595CQ4A122t17E7Q5DFB18367dF20538338.ics';
+app.use(express.static('public'))
+app.use(compression())
+app.use(helmet())
+app.set("view engine", "ejs")
 
-const removeCourses = ['EDA452', 'RRY125', 'EEM021', 'FUF045', 'MVE550', 'TME055', 'TMA690', 'FTF131', "MVE370"];
+//const url = 'https://cloud.timeedit.net/chalmers/web/public/ri6Y036mZ55Z6hQ1W55865615Q40y4Q6Zt680ZZZX46Q627695y0nZ65QZA1D6C7tZCD595CQ4A122t17E7Q5DFB18367dF20538338.ics';
+
+//const removeCourses = ['EDA452', 'RRY125', 'EEM021', 'FUF045', 'MVE550', 'TME055', 'TMA690', 'FTF131', "MVE370"];
 
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.sendFile(path.join(__dirname, '/index.html'));
 })
+
+app.get("/register",(req, res) => {
+  let url = req.query.timeEditUrl
+  if (url.startsWith("https://cloud.timeedit.net/")) {
+    let userid = uid()
+    res.redirect("/config/" + userid)
+    config[userid] = {}
+    config[userid]["url"] = url
+    fs.writeFileSync('config.json', JSON.stringify(config, null, 2));
+  } else {
+    res.send("Invalid URL")
+  }
+})
+
+app.get("/config/:user", (req, res) => {
+  let user = req.params["user"]
+  let url = config[user]["url"]
+  res.render("config", {url: url, user: user})
+});
 
 app.get("/cal/:user.ics", async (req, res) => {
   let user = req.params["user"]
   console.log(user)
-  if(user == "emrik"){
-    let toSend = await fetchyFilter(url)
+  if(config.user && config.user.url){
+    let toSend = await fetchyFilter(config.user)
     res.set({"Content-Disposition":"attachment; filename=" + user + ".ics",'Content-type': 'text/calendar'});
     res.send(toSend);
   } else {
@@ -29,7 +58,8 @@ app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
 });
 
-const filterEvents = (events) => {
+const filterEvents = (events, user) => {
+  const removeCourses = user.removeCourses;
   // Plaintext Replacement
   let plaintext = JSON.stringify(events);
 
@@ -62,7 +92,8 @@ const filterEvents = (events) => {
 };
 
 // Fetch and filter and to serve it back to the client
-const fetchyFilter = async (url) => {
+const fetchyFilter = async (user) => {
+  const url = user.url;
   try {let response = await got(url);
     if (response.statusCode != 200) {
       console.error('Failed to fetch calendar');
@@ -72,7 +103,7 @@ const fetchyFilter = async (url) => {
     output.VCALENDAR[0].PRDOID = "-//TimeEditEd\\\\\\, //TimeEditEd//EN";
     output.VCALENDAR[0]["X-WR-CALNAME"] = output.VCALENDAR[0]["X-WR-CALNAME"].replace('TimeEdit-', '');
     const events = output.VCALENDAR[0].VEVENT;
-    const filteredEvents = await filterEvents(events);
+    const filteredEvents = await filterEvents(events, user);
     output.VCALENDAR[0].VEVENT = filteredEvents;
     // fs.writeFileSync('events.json', JSON.stringify(events, null, 2));
     // fs.writeFileSync('eventsfilter.json', JSON.stringify(filteredEvents, null, 2));
